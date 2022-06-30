76b5ecf9f8120c4e95029387e1498f3926ae9cd3
gabl <gabl22ytmc@gmail.com>
2022-06-30 21:44:04 +0200

    Updated project files

--- core/src/main/java/de/natrox/configuration/ConfigCluster.java
+++ core/src/main/java/de/natrox/configuration/ConfigCluster.java
@@ -1,14 +1,27 @@
+/*
+ * Copyright 2020-2022 NatroxMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.natrox.configuration;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
 
 public class ConfigCluster {
 
@@ -25,7 +38,7 @@ public class ConfigCluster {
             String[] dropped = new String[key.length - 1];
             System.arraycopy(key, 1, dropped, 0, key.length - 1);
             this.getCluster(key[0]).add(dropped, value);
-        } else if (key.length == 1)
+        } else if(key.length == 1)
             properties.put(key[0], ConfigElement.of(value));
         return this;
     }
